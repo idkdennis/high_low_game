3944e7e55ea1c3ba5ce40cee1be3452c2753169a
Dennis raju <154963240+idkdennis@users.noreply.github.com>
2026-01-29 23:26:15 +0530

    Add follower comparison game logic
    Implement a game that compares followers of two entities.

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+from art import logo
+from gamedata import data
+
+score = 0
+winner = random.randint(0, 50)
+
+while True:
+    print(logo)
+
+    b = random.randint(0, 50)
+    while b == winner:
+        b = random.randint(0, 50)
+
+    print(
+        f"Compare A: {data[winner]['name']} a "
+        f"{data[winner]['description']} "
+        f"from {data[winner]['country']}"
+    )
+
+    print(vs)
+
+    print(
+        f"Against B: {data[b]['name']} a "
+        f"{data[b]['description']} "
+        f"from {data[b]['country']}"
+    )
+
+    choice = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    c = data[winner]['follower_count']
+    d = data[b]['follower_count']
+
+    def checker(c, d):
+        if c > d:
+            return "a"
+        elif c < d:
+            return "b"
+
+    t = checker(c, d)
+
+    if choice == t:
+        if t == 'b':
+            winner = b
+        score += 1
+        print("\n" * 50)
+        print(f"your score is :{score}")
+    else:
+        print("wrong answer")
+        print(f"your score is :{score}")
+        break
